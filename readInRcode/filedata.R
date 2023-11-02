edc8c82e17fff2ddcb76bdd962494ffceae18419
Yushu Shi <36617896+YushuShi@users.noreply.github.com>
2023-11-01 23:10:25 -0400

    Add files via upload

--- readInRcode/filedata.R
+++ readInRcode/filedata.R
@@ -1,15 +1,15 @@
 filedata <- reactive({
   infile <- input$datafile
   if (is.null(infile)) {
-    if(input$CoxorFG=="noComp"){
-      if(input$intCen=="noint"){
-        read.csv(ifelse(input$strata=="yes","veteran2.csv","veteran.csv"), stringsAsFactors = FALSE,row.names = 1)
+    if(identical(input$CoxorFG,"noComp")){
+      if(identical(input$intCen,"noint")){
+        read.csv(ifelse(identical(input$strata,"yes"),"veteran2.csv","veteran.csv"), stringsAsFactors = FALSE,row.names = 1)
       }else{
-        read.csv(ifelse(input$strata=="yes","bcdeter2.csv","bcdeter2.csv"), stringsAsFactors = FALSE,row.names = 1)   
-        # read.csv(ifelse(input$strata=="yes","bcdeter2.csv","bcdeter.csv"), stringsAsFactors = FALSE,row.names = 1) 
+        read.csv(ifelse(identical(input$strata,"yes"),"bcdeter2.csv","bcdeter2.csv"), stringsAsFactors = FALSE,row.names = 1)   
+
       }
     }else{
-      if(input$intCen=="noint"){
+      if(identical(input$intCen,"noint")){
         read.csv("Paquid.csv", stringsAsFactors = FALSE,row.names = 1)
       }else{
         read.csv("sim1234.csv", stringsAsFactors = FALSE,row.names = 1)  
