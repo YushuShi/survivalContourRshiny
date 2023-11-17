c222712e84924be7995288fe1820ebbdfec296f4
Yushu Shi <36617896+YushuShi@users.noreply.github.com>
2023-11-16 23:24:23 -0500

    Add files via upload

--- readInRcode/filedata.R
+++ readInRcode/filedata.R
@@ -3,18 +3,18 @@ filedata <- reactive({
   if (is.null(infile)) {
     if(identical(input$CoxorFG,"noComp")){
       if(identical(input$intCen,"noint")){
-        read.csv(ifelse(identical(input$strata,"yes"),"veteran2.csv","veteran.csv"), stringsAsFactors = FALSE,row.names = 1)
+        read.csv(ifelse(identical(input$strata,"yes"),"veteran2.csv","veteran.csv"), stringsAsFactors = TRUE,row.names = 1)
       }else{
-        read.csv(ifelse(identical(input$strata,"yes"),"bcdeter2.csv","bcdeter2.csv"), stringsAsFactors = FALSE,row.names = 1)   
+        read.csv(ifelse(identical(input$strata,"yes"),"bcdeter2.csv","bcdeter2.csv"), stringsAsFactors = TRUE,row.names = 1)   
 
       }
     }else{
       if(identical(input$intCen,"noint")){
-        read.csv("Paquid.csv", stringsAsFactors = FALSE,row.names = 1)
+        read.csv("Paquid.csv", stringsAsFactors = TRUE,row.names = 1)
       }else{
-        read.csv("sim1234.csv", stringsAsFactors = FALSE,row.names = 1)  
+        read.csv("sim1234.csv", stringsAsFactors = TRUE,row.names = 1)  
       }
     }
   }else{
-    read.csv(infile$datapath,stringsAsFactors = FALSE)}
+    read.csv(infile$datapath,stringsAsFactors = TRUE)}
 })
