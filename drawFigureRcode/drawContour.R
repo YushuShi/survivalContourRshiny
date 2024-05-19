64eecf91571ab875d321c84840f560f70977d0cc
Yushu Shi <36617896+YushuShi@users.noreply.github.com>
2024-05-19 17:19:02 -0400

    Update drawContour.R

--- drawFigureRcode/drawContour.R
+++ drawFigureRcode/drawContour.R
@@ -7,7 +7,7 @@ output$drawContour<- renderPlotly({
   if(!identical(input$strata,"yes")){
     histData<-dataTable[,input$covariate]
     fig <- plot_ly(x = predictPlot$time, y = contcov, z= t(predictPlot$surv),
-                   colorbar = list(title =                                    ifelse(identical(input$CoxorFG,"noComp"),                                                                              "Predicted survival probability","Predicted CIF"),
+                   colorbar = list(title = list(text=ifelse(identical(input$CoxorFG,"noComp"),                                                                              "Predicted survival probability","Predicted CIF")),
                                    titleside='right'),
                    colorscale=input$colSche,type = "contour",
                    hovertemplate = paste('At time %{x:.2f} <br>with',showCont,
@@ -17,17 +17,27 @@ output$drawContour<- renderPlotly({
     fig <- fig %>% layout(title=list(text=ifelse(identical(input$CoxorFG,"noComp"),
                                          "Contour Plot of the Predicted Survival Probability",
                          "Contour Plot of the Predicted Cumulative Incidence Function"),
+                         font=list(size=20),
                          x=0.15),
-                  xaxis=list(title="Time",range=range(predictPlot$time)),
-                  yaxis=list(title=showCont,range=range(contcov)))
-    
-    s <- subplot(fig,
-      plot_ly(y = histData, type = "histogram",hoverinfo='none') %>% layout(xaxis=list(title="Count")),
-      nrows = 1, widths = c(0.8, 0.2), margin = 0.01,
-      shareY = TRUE,
-      titleX=TRUE,
-      titleY=TRUE
-    )
+                  xaxis=list(title=list(text="Time",
+                                        font=list(size=20)),range=range(predictPlot$time)),
+                  yaxis=list(title=list(text=showCont,
+                                        font=list(size=20)),range=range(contcov)))
+    histPlot<-plot_ly(y = histData, type = "histogram",hoverinfo='none') 
+    histPlot<-histPlot%>%layout(margin=list(l=100))
+   s <- subplot(fig,
+     histPlot %>% layout(xaxis=list(title="Count")),
+     nrows = 1, widths = c(0.8, 0.2),
+     margin = 0.01,
+     shareY = TRUE,
+     titleX=TRUE,
+     titleY=TRUE
+   )
+   
+   s <- s %>% layout(
+     margin = list(t = 50,l=5) # Adjust top margin to ensure enough space for the title
+   )
+   
   }else{
     # for now we only have stratified Cox model, no stratified FG model.
     strataList<-unique(dataTable[,input$strataCov])
@@ -37,14 +47,17 @@ output$drawContour<- renderPlotly({
       histData<-dataTable[dataTable[,input$strataCov]==strataList[i],input$covariate]
       histData<-histData[!is.na(histData)]
       temp <- plot_ly(x = predictPlot$time, y = contcov, z= t(predictPlot$surv[[i]]),
-                      colorbar = list(title =                                    ifelse(identical(input$CoxorFG,"noComp"),                                                                              "Predicted survival probability","Predicted CIF"),
+                      colorbar = list(title =list(text=ifelse(identical(input$CoxorFG,"noComp"),                                                                              "Predicted survival probability","Predicted CIF")),
                                       titleside='right'),
                       colorscale=input$colSche,type = "contour",
                      hovertemplate = paste('At time %{x:.2f} <br>with',showCont,
                                            'being %{y:.2f},<br>the predicted survival is %{z:.2f}<extra></extra>'))
-      temp <- temp %>% layout(title=list(text="Contour Plot of the Predicted Survival Probability",x=0.15),
-        xaxis=list(title="Time",range=range(predictPlot$time)),
-                            yaxis=list(title=showCont,range=range(contcov)))
+      temp <- temp %>% layout(title=list(text="Contour Plot of the Predicted Survival Probability",
+                                         font = list(size = 20),
+                                         x=0.15),
+        xaxis=list(title=list(text="Time",font = list(size = 20)),range=range(predictPlot$time)),
+                            yaxis=list(title=list(text=showCont,
+                                                  font = list(size = 20)),range=range(contcov)))
       if(i>1){
         temp<-temp %>% hide_colorbar()
       }
@@ -66,7 +79,9 @@ output$drawContour<- renderPlotly({
                  margin=c(0,0.01,0.02,0),
                  shareX=TRUE,
                  shareY=TRUE,titleX = TRUE,titleY = TRUE)
-    s<-s%>%layout(height=450*length(strataList))
+    s<-s%>%layout(height=450*length(strataList),
+                     margin = list(t = 50,l=5) # Adjust top margin to ensure enough space for the title
+                   )
   }
   s
 })
